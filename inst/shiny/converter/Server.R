892dc60d26b83f1f2ae56ad6b2d6a8d32ed7c4f1
tzerk <christoph.burow@gmx.net>
2015-01-27 18:45:29 +0100

    use renamed function

--- inst/shiny/converter/Server.R
+++ inst/shiny/converter/Server.R
@@ -1,7 +1,4 @@
 ## Server.R
-
-
-
 ## MAIN FUNCTION
 shinyServer(function(input, output, session) {
   
@@ -23,18 +20,15 @@ shinyServer(function(input, output, session) {
     
     new_paths <- vector(mode = "character", length = length(file$name))
     
-    print(file$datapath)
-    
     for (i in seq_along(new_paths)) {
       new_paths[i] <- paste0(gsub("/[^/]*$", "", file$datapath[i]), "/", file$name[i])
       file.rename(file$datapath[i], new_paths[i])
     }
-    print(new_paths)
     # strip .PAR and .DSC files
     new_paths <- new_paths[-grep("(.par$)|(.dsc$)", new_paths, ignore.case = TRUE)]
     new_names <- file$name[-grep("(.par$)|(.dsc$)", file$name, ignore.case = TRUE)]
     
-    res <- lapply(new_paths, read_ESRspectrum)
+    res <- lapply(new_paths, read_Spectrum)
     
     for (i in seq_along(res)) {
       data <- as.data.frame(res[[i]]$data)
