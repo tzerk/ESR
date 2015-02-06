54f5a2529c4a56a97b467963544f5c24b0258d6d
tzerk <christoph.burow@gmx.net>
2015-02-06 17:23:03 +0100

    generic conversion function

---
+++ R/utils.R
@@ -0,0 +1,17 @@
+#' Coercion from and to ESR.Spectrum
+#' 
+#' A generic function \code{as.ESR.Spectrum} for coercing objects
+#' to class "\code{ESR.Spectrum}".
+#' 
+#' \code{as.ESR.Spectrum} currently includes methods for \link{data.frame} objects.
+#' 
+#' @param x an object
+#' @return \code{as.ESR.Spectrum} returns an \code{ESR.Spectrum} object.
+#' @seealso \link{R6}, \link{R6Class}
+#' @author Christoph Burow, University of Cologne (Germany)
+#' @export as.ESR.Spectrum
+#' @examples
+#' ## TODO
+as.ESR.Spectrum <- function(x) {
+  
+}
