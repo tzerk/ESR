a14990ed87ec0786a0f90331b90116131ddfb3d9
tzerk <christoph.burow@gmx.net>
2015-02-06 17:23:24 +0100

    improve UI & fix for single files

--- inst/shiny/converter/Server.R
+++ inst/shiny/converter/Server.R
@@ -2,21 +2,17 @@
 ## MAIN FUNCTION
 shinyServer(function(input, output, session) {
   
+  rval <- reactiveValues(conv_files = NULL)
+  
   datGet <- reactive({
     inFile <- input$files
-    
-    if (is.null(inFile))
-      return(NULL)
-    
+    if (is.null(inFile)) return(NULL)
     return(inFile)
   })
 
-  
- output$js <- renderText({
-    input$btn
-    
-    isolate({
+  observeEvent(input$btn, {
     file <- datGet()
+    if (is.null(file)) return(NULL)
     
     new_paths <- vector(mode = "character", length = length(file$name))
     
@@ -25,19 +21,25 @@ shinyServer(function(input, output, session) {
       file.rename(file$datapath[i], new_paths[i])
     }
     # strip .PAR and .DSC files
-    new_paths <- new_paths[-grep("(.par$)|(.dsc$)", new_paths, ignore.case = TRUE)]
-    new_names <- file$name[-grep("(.par$)|(.dsc$)", file$name, ignore.case = TRUE)]
+    rm1 <- grep("(.par)|(.dsc)", new_paths, ignore.case = TRUE)
+    if (length(rm1) != 0) new_paths <- new_paths[-rm1]
+    rm2 <- grep("(.par)|(.dsc)", file$name, ignore.case = TRUE)
+    if(length(rm2) != 0) new_names <- file$name[-rm2]
+    else new_names <- file$name
     
     res <- lapply(new_paths, read_Spectrum)
     
     for (i in seq_along(res)) {
       data <- as.data.frame(res[[i]]$data)
-      dest <- paste0(input$todir,"/conv_", gsub("...$", "", new_names[i]), "txt")
-      
+      dest <- paste0(input$todir, input$prefix, gsub("....$", "", new_names[i]), input$suffix, ".txt")
       write.table(data, dest, row.names = FALSE, quote = FALSE)
     }
-    
-    })
+    rval$conv_files <- new_names
+  })
+  
+ output$js <- renderText({
+     if (!is.null(rval$conv_files))
+       paste(rval$conv_files, collapse = "</br>")
  })
 
 })##EndOf::shinyServer(function(input, output)
--- inst/shiny/converter/UI.R
+++ inst/shiny/converter/UI.R
@@ -7,12 +7,19 @@
 
 pageWithSidebar(  
   # 1 - title = NULL -> Panel will not be shown
-  headerPanel(title = "File converter"),
+  headerPanel(title = "ESR file converter"),
   sidebarPanel(width = 4,
                
                fileInput('files', 'Choose spectrum files', multiple = TRUE,
                          accept=c(".txt", ".dta", ".dsc", ".spc", ".par", ".asc")),
-               
+               fluidRow(
+                 column(width = 6, 
+                        textInput("prefix", "Prefix", "")
+                        ),
+                 column(width = 6,
+                        textInput("suffix", "Suffix", "_conv")
+                        )
+               ),
                textInput("todir", "Save in directory...", value = file.path(Sys.getenv("USERPROFILE"),"Desktop")),
                
                actionButton("btn", "Convert!")
@@ -20,8 +27,13 @@ pageWithSidebar(
   
   # 3 - output panel
   mainPanel(width = 8,
-            
-            textOutput("js"),
+            helpText(HTML("<h4><b>A simple file converter for Bruker spectrum files</b></h4></br>
+                          File formats of the following Bruker ESR spectrometers are currently supported:</br>
+                          <li>ESP300-E X-band-Spectrometer (<code>.SPC</code>&<code>.PAR</code> extension)</li>
+                          <li>ELEXSYS500 X-band Spectromete (<code>.DAT</code>&<code>.DSC</code> extension)</li>")
+            ),
+            hr(),
+            htmlOutput("js"),
             
             # insert css code inside <head></head> of the generated HTML file:
             # allow open dropdown menus to reach over the container
